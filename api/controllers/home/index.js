e7d631e374679f8ffeb52be0e7d26ebec0985569
Lennox Charles <lennyaiko17@gmail.com>
2024-03-09 23:39:34 +0100

    progress

--- api/controllers/home/index.js
+++ api/controllers/home/index.js
@@ -12,10 +12,34 @@ module.exports = {
   },
 
   fn: async function () {
-    const page = await fetch('https://clenny.tech')
+    var page
+    const maxRetries = 3
+    let retries = 0
+
+    async function fetchData() {
+      try {
+        page = await fetch('https://lendocredit.com')
+      } catch (error) {
+        if (retries < maxRetries) {
+          retries++
+          await fetchData()
+        } else {
+          sails.log(error)
+        }
+      }
+    }
+
+    try {
+      await fetchData()
+    } catch (error) {
+      sails.log('Error fetching data:', error)
+    }
 
     const htmlText = await page.text()
 
-    return { page: 'index', props: { htmlText } }
+    return {
+      page: 'index',
+      props: { htmlText, baseUrl: 'https://clenny.tech' }
+    }
   }
 }
--- assets/js/pages/index.vue
+++ assets/js/pages/index.vue
@@ -4,21 +4,38 @@ import { parse } from 'node-html-parser'
 
 import { Link, Head, usePage } from '@inertiajs/vue3'
 
-const { htmlText } = defineProps({
-  htmlText: { type: String }
+const { htmlText, baseUrl } = defineProps({
+  htmlText: { type: String },
+  baseUrl: { type: String }
 })
 
 const page = parse(htmlText)
 
-const onclickBtn = () => {
-  console.log(page.getElementsByTagName('meta'))
-}
+watcher()
+
+const image = ref(
+  page.querySelector('meta[property="og:image"]').getAttribute('content')
+)
+const url = new URL(baseUrl).host
+const title = ref(
+  page.querySelector('meta[property="og:title"]').getAttribute('content')
+)
+
+const description = ref(
+  page.querySelector('meta[property="og:description"]').getAttribute('content')
+)
 </script>
 
 <template>
   <Head title="Mellow Vue"></Head>
   <main>Hi there</main>
 
-  <button @click="onclickBtn" class="">Click me</button>
+  <div v-if="htmlText" class="">
+    <button @click="onclickBtn" class="p-2 m-2 border">Click me</button>
+    <img :src="image" alt="" class="" />
+    <p class="">{{ url }}</p>
+    <p class="">{{ title }}</p>
+    <p class="">{{ description }}</p>
+  </div>
 </template>
 <style></style>
